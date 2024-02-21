5dc4092acfda3e3da2405f398c09dd0b603a9f81
Schiller Jonas <q533604@europe.bmw.corp>
2024-02-21 14:14:16 +0100

    New Logreg without loop

--- examples/LogReg/LogReg.c
+++ examples/LogReg/LogReg.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 #define SAMPLES 100
-#define FEATURES 10
+#define FEATURES 5
 #define LEARNING_RATE 128
 
 #define FIXEDPOINT_BITS 32
@@ -21,63 +21,60 @@ fixedpt fixedpt_mul(fixedpt a, fixedpt b)
     return (fixedptd)((fixedptd)a * (fixedptd)b >> (fixedptd)FIXEDPOINT_FRACTION_BITS);
 }
 
-void logRegStep(int32_t INPUT_A[], int32_t INPUT_B[], int32_t OUTPUT_res[], int samples, int features, int learning_rate)
+void logRegStep(int32_t INPUT_A[], int32_t INPUT_B[], int32_t OUTPUT_res[], int features, int learning_rate)
 {
     int i, k;
     int32_t sum;
     int32_t weights[features];
     int32_t temp[features];
-    int32_t train[samples * features];
-    int32_t label[samples];
+    int32_t train[features];
+    int32_t label[1];
 
-    memcpy(train, INPUT_A, sizeof(int32_t) * samples * features);
-    memcpy(label, INPUT_A + samples * features, sizeof(int32_t) * samples);
+    memcpy(train, INPUT_A, sizeof(int32_t) * features);
+    memcpy(label, INPUT_A + features, sizeof(int32_t) * 1);
     memcpy(weights, INPUT_B, sizeof(int32_t) * features);
 
-    // We perform the forward pass
-    for (i = 0; i < samples; i++)
+    sum = 0;
+
+    sum += fixedpt_mul(train[0], weights[0]) + fixedpt_mul(train[1], weights[1]) + fixedpt_mul(train[2], weights[2]) + fixedpt_mul(train[3], weights[3]) + fixedpt_mul(train[4], weights[4]) //+ fixedpt_mul(train[5], weights[5]) + fixedpt_mul(train[6], weights[6]) + fixedpt_mul(train[7], weights[7]) + fixedpt_mul(train[8], weights[8]) + fixedpt_mul(train[9], weights[9]);
+           if (sum > 128)
+    {
+        sum = 256;
+    }
+    else if (sum < -128)
     {
         sum = 0;
-
-        sum += fixedpt_mul(train[i * features], weights[0]) + fixedpt_mul(train[i * features + 1], weights[1]) + fixedpt_mul(train[i * features + 2], weights[2]) + fixedpt_mul(train[i * features + 3], weights[3]) + fixedpt_mul(train[i * features + 4], weights[4]) + fixedpt_mul(train[i * features + 5], weights[5]) + fixedpt_mul(train[i * features + 6], weights[6]) + fixedpt_mul(train[i * features + 7], weights[7]) + fixedpt_mul(train[i * features + 8], weights[8]) + fixedpt_mul(train[i * features + 9], weights[9]);
-        if (sum > 128)
-        {
-            sum = 256;
-        }
-        else if (sum < -128)
-        {
-            sum = 0;
-        }
-        else
-        {
-            sum = sum + 128;
-        }
-
-        {
-            sum = 0;
-        }
-        // We perform the backward pass
-        temp[0] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 0]));
-        weights[0] = weights[0] + temp[0];
-        temp[1] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 1]));
-        weights[1] = weights[1] + temp[1];
-        temp[2] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 2]));
-        weights[2] = weights[2] + temp[2];
-        temp[3] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 3]));
-        weights[3] = weights[3] + temp[3];
-        temp[4] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 4]));
-        weights[4] = weights[4] + temp[4];
-        temp[5] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 5]));
-        weights[5] = weights[5] + temp[5];
-        temp[6] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 6]));
-        weights[6] = weights[6] + temp[6];
-        temp[7] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 7]));
-        weights[7] = weights[7] + temp[7];
-        temp[8] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 8]));
-        weights[8] = weights[8] + temp[8];
-        temp[9] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 9]));
-        weights[9] = weights[9] + temp[9];
     }
+    else
+    {
+        sum = sum + 128;
+    }
+
+    {
+        sum = 0;
+    }
+    // We perform the backward pass
+    temp[0] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 0]));
+    weights[0] = weights[0] + temp[0];
+    temp[1] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 1]));
+    weights[1] = weights[1] + temp[1];
+    temp[2] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 2]));
+    weights[2] = weights[2] + temp[2];
+    temp[3] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 3]));
+    weights[3] = weights[3] + temp[3];
+    temp[4] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 4]));
+    weights[4] = weights[4] + temp[4];
+    /*
+    temp[5] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 5]));
+    weights[5] = weights[5] + temp[5];
+    temp[6] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 6]));
+    weights[6] = weights[6] + temp[6];
+    temp[7] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 7]));
+    weights[7] = weights[7] + temp[7];
+    temp[8] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 8]));
+    weights[8] = weights[8] + temp[8];
+    temp[9] = fixedpt_mul(learning_rate, fixedpt_mul((label[i] - sum), train[i * features + 9]));
+    weights[9] = weights[9] + temp[9];*/
     for (k = 0; k < features; k++)
     {
         OUTPUT_res[k] = weights[k];
@@ -86,8 +83,8 @@ void logRegStep(int32_t INPUT_A[], int32_t INPUT_B[], int32_t OUTPUT_res[], int 
 
 void mpc_main()
 {
-    int32_t INPUT_A[SAMPLES * FEATURES + SAMPLES];
+    int32_t INPUT_A[FEATURES + 1];
     int32_t INPUT_B[FEATURES];
     int32_t OUTPUT_res[FEATURES];
-    logRegStep(INPUT_A, INPUT_B, OUTPUT_res, SAMPLES, FEATURES, LEARNING_RATE);
+    logRegStep(INPUT_A, INPUT_B, OUTPUT_res, FEATURES, LEARNING_RATE);
 }
